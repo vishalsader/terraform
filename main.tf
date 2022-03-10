a6799b07574cad078db63a0789f8c21ce8e4d424
vishalsader <85609829+vishalsader@users.noreply.github.com>
2022-03-10 20:09:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,3 +14,7 @@ resource "azurerm_resource_group" "rg_vishal_dev" {
   name     = "rg_vishal_dev"
   location = "Central India"
 }
+resource "azurerm_resource_group" "rg_vishal_qa" {
+  name     = "rg_vishal_qa"
+  location = "Central India"
+}
