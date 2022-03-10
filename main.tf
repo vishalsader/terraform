3ec363dca1f9da7958235081653f93c74a01f19c
vishalsader <85609829+vishalsader@users.noreply.github.com>
2022-03-10 19:58:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg_vishal_dev" {
+  name     = "rg_vishal_dev"
+  location = "Central India"
+}
