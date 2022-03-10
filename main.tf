484511c3a59bec04f351abd7ff1d3b6e318edf81
vishalsader <85609829+vishalsader@users.noreply.github.com>
2022-03-10 20:16:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "azurerm_resource_group" "rg_vishal_qa" {
   name     = "rg_vishal_qa"
   location = "Central India"
 }
+resource "azurerm_app_service_plan" "dev_plan_appservice" {
+  name                = "dev_plan_appservice"
+  location            = azurerm_resource_group.rg_vishal_dev.location
+  resource_group_name = azurerm_resource_group.rg_vishal_dev.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
